a6a39e0f737795f92f57529cf2b5614e7a88fab7
Andrei <6abc6@mail.ru>
2021-01-31 11:33:25 +0300

    create MovieDetails

--- MovieDBApp/Models/Movie.swift
+++ MovieDBApp/Models/Movie.swift
@@ -16,6 +16,24 @@ struct Movie: Decodable, Identifiable {
     let voteAverage:Double
     let voteCount: Int
     let runtime: Int?
+    let releaseDate: String?
+    
+    let genres: [MovieGenre]?
+    let credits: MovieCredit?
+    
+    static private var yearFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        return formatter
+    }()
+    
+    static private let durationFormatter: DateComponentsFormatter = {
+        
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .full
+        formatter.allowedUnits = [.hour, .minute]
+        return formatter
+    }()
     
     var backDropURL: URL {
         return URL(string: "https://image.tmbd.org/t/p/w500\(backdropPath ?? "")")!
@@ -24,5 +42,77 @@ struct Movie: Decodable, Identifiable {
     var posterURL: URL {
         return URL(string: "https://image.tmbd.org/t/p/w500\(posterPath ?? "")")!
     }
+    var genreText: String {
+        genres?.first?.name ?? "n/a"
+    }
+    
+    var raitingText: String {
+        let rating = Int(voteAverage)
+        let ratingText = (0..<rating).reduce("") { (acc, _) -> String in
+            
+            
+            return acc + "⭐️"
+    }
+        return ratingText
+    }
+    
+    var scoreText: String  {
+        guard raitingText.count > 0 else {
+            return "n/a"
+        }
+        return "\(raitingText.count)/10"
+    }
+    
+    var yearText: String {
+        guard let releaseDate = self.releaseDate, let date = Utilities.dateFormatter.date(from: releaseDate) else {
+            return "n/a"
+        }
+        return Movie.yearFormatter.string(from: date)
+    }
+    
+    var durationText: String {
+        guard let runtime = self.runtime, runtime > 0 else {
+            return "n/a"
+        }
+        return Movie.durationFormatter.string(from: TimeInterval(runtime) * 60) ?? "n/a"
+        
+    }
+    var casts: [MovieCast]? {
+        credits?.cast
+    }
+    var crew: [MovieCrew]? {
+        credits?.crew
+    }
+    var directors: [MovieCrew]? {
+        crew?.filter { $0.job.lowercased() == "director"}
+    }
+    var produsers: [MovieCrew]? {
+        crew?.filter { $0.job.lowercased() == "produser"}
+    }
+    
+    var screeWriter: [MovieCrew]? {
+        crew?.filter { $0.job.lowercased() == "story"}
+    }
+}
+
+struct MovieGenre: Decodable {
+    let name: String
+}
+
+struct MovieCredit: Decodable {
+    let cast: [MovieCast]
+    let crew: [MovieCrew]
+}
+
+struct MovieCast: Decodable {
+    let id: Int
+    let character: String
+    let name: String
+}
+
+struct MovieCrew: Decodable {
+    let id: Int
+    let job: String
+    let name: String
     
 }
--- MovieDBApp/Views/MovieDetails.swift
+++ MovieDBApp/Views/MovieDetails.swift
@@ -2,25 +2,23 @@
 import SwiftUI
 
 struct MovieDetails: View {
-    
     let movieID: Int
-    @ObservedObject private var movieDetailsState = MOvieDetailState()
-    
-    
-    
+    @ObservedObject private var movieDetailsState = MovieDetailState()
     
     var body: some View {
-        ZStack{
+        ZStack {
             LoadView(isLoading: self.movieDetailsState.isLoading, error: self.movieDetailsState.error) {
                 self.movieDetailsState.loadMovie(id: self.movieID)
             }
+            if movieDetailsState.movie != nil {
+                MovieDetailListView(movie: self.movieDetailsState.movie!)
+                   
+            }
         }
-        
         .navigationBarTitle(movieDetailsState.movie?.title ?? "" )
         .onAppear {
             self.movieDetailsState.loadMovie(id: self.movieID)
         }
-        
     }
 }
 
@@ -30,13 +28,46 @@ struct MovieDetailListView: View {
     
     var body: some View {
         List {
+            MovieDetailImage(imageURL: self.movie.backDropURL)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             
+            HStack {
+                Text(movie.genreText)
+                Text(".")
+                Text(movie.yearText)
+                Text(movie.durationText)
+            }
+            Text(movie.overview)
+            HStack {
+                if movie.raitingText.isEmpty {
+                    Text(movie.durationText).foregroundColor(.yellow)
+                }
+                Text(movie.scoreText)
+            }
+            Divider()
         }
     }
-    
-    
 }
 
+struct MovieDetailImage: View {
+    
+    @ObservedObject private var imageLoader = ImageLoader()
+    let imageURL: URL
+    
+    var body: some View {
+        ZStack {
+            Rectangle().fill(Color.gray.opacity(0.3))
+            if self.imageLoader.image != nil {
+                Image(uiImage: self.imageLoader.image!)
+                    .resizable()
+            }
+        }
+        .aspectRatio(16/9, contentMode: .fit)
+        .onAppear {
+            self.imageLoader.loadImage(with: self.imageURL)
+        }
+    }
+}
 
 struct MovieDetails_Previews: PreviewProvider {
     static var previews: some View {
